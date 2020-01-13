0d4779cdff63d5db968915ca0de362ee9f1e8e89
wonsik <cws0173@gmail.com>
2020-01-13 12:03:29 +0900

    update userDao, add userDaoTest

--- src/com/handong/dao/UserDao.java
+++ src/com/handong/dao/UserDao.java
@@ -4,12 +4,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.sql.DriverManager;
 
 public class UserDao {
 	private Connection conn;
-    private PreparedStatement pstmt;  
-    private ResultSet rs;
     
     public UserDao() {
         try {
@@ -26,17 +25,112 @@ public class UserDao {
     public int join(User user) {
         String SQL= "INSERT INTO user VALUES(?, ?, ?, ?, ?) ";
         try {
-            pstmt = conn.prepareStatement(SQL);
+            PreparedStatement pstmt = conn.prepareStatement(SQL);
             pstmt.setString(1,user.getUserID());
             pstmt.setString(2,user.getUserPassword());
             pstmt.setString(3,user.getUserName());
             pstmt.setString(4,user.getUserGender());
             pstmt.setString(5,user.getUserEmail());
-            return pstmt.executeUpdate();
+            return pstmt.executeUpdate();	// return 1
         }
-        catch(Exception e) {
-            e.printStackTrace();
+        catch(SQLException e) {
+        	return -1;
         }
-        return -1;
+    }
+    
+    public int update(User user) {
+    	String sql = "UPDATE user SET userPassword=?, userName=?, userGender=?, userEmail=? WHERE userId=?";
+    	
+    	try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,user.getUserPassword());
+            pstmt.setString(2,user.getUserName());
+            pstmt.setString(3,user.getUserGender());
+            pstmt.setString(4,user.getUserEmail());
+            pstmt.setString(5,user.getUserID());
+
+            return pstmt.executeUpdate();	// return 1
+        }
+        catch(SQLException e) {
+            return -1;
+        }
+    }
+    
+    public int delete(User user) {
+    	String sql = "DELETE FROM user WHERE userId=?";
+    	
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,user.getUserID());
+            return pstmt.executeUpdate();	// return 1
+        }
+        catch(SQLException e) {
+            return -1;
+        }
+    }
+    
+    public int deleteAll() {
+    	String sql = "DELETE FROM user";
+    	
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(sql);
+            return pstmt.executeUpdate();	// return 1
+        }
+        catch(SQLException e) {
+//            e.printStackTrace();
+            return -1;
+        }
+    }
+    
+    public User search(String userId) {
+    	String sql = "SELECT * FROM user WHERE userId=?";
+    	User u = new User();
+    	
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1,userId);
+            ResultSet rs = pstmt.executeQuery();
+            
+            if(!rs.next()) {
+            	return null;
+            }
+            
+            u.setUserID(rs.getString("userId"));
+            u.setUserPassword(rs.getString("userPassword"));
+            u.setUserName(rs.getString("userName"));
+            u.setUserGender(rs.getString("userGender"));
+            u.setUserEmail(rs.getString("userEmail"));
+            
+            return u;
+        }
+        catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    public ArrayList<User> searchAll() {
+    	String sql = "SELECT * FROM user ORDER BY userId ASC";
+    	ArrayList<User> list = new ArrayList<User>();
+    	User u = null;
+    	try {
+    		PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            
+            while(rs.next()) {
+            	u = new User();
+            	u.setUserID(rs.getString("userId"));
+            	u.setUserPassword(rs.getString("userPassword"));
+            	u.setUserName(rs.getString("userName"));
+            	u.setUserGender(rs.getString("userGender"));
+            	u.setUserEmail(rs.getString("userEmail"));
+            	
+            	list.add(u);
+            }
+            
+            return list;
+        }
+        catch(SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/com/handong/dao/UserDaoTest.java
@@ -0,0 +1,120 @@
+package com.handong.dao;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class UserDaoTest {
+	UserDao userDao;
+	ArrayList<User> list;
+	
+	@BeforeEach
+	public void setup() {
+		userDao = new UserDao();
+		userDao.deleteAll();
+		
+		User u1 = new User();
+		u1.setUserID("id1");
+		u1.setUserPassword("pw1");
+		u1.setUserEmail("email1");
+		u1.setUserName("name1");
+		u1.setUserGender("m");
+		
+		User u2 = new User();
+		u2.setUserID("id2");
+		u2.setUserPassword("pw2");
+		u2.setUserEmail("email2");
+		u2.setUserName("name2");
+		u2.setUserGender("f");
+		
+		User u3 = new User();
+		u3.setUserID("id3");
+		u3.setUserPassword("pw3");
+		u3.setUserEmail("email3");
+		u3.setUserName("name3");
+		u3.setUserGender("m");
+		
+		User u4 = new User();
+		u4.setUserID("id4");
+		u4.setUserPassword("pw4");
+		u4.setUserEmail("email4");
+		u4.setUserName("name4");
+		u4.setUserGender("f");
+		
+		User u5 = new User();
+		u5.setUserID("id5");
+		u5.setUserPassword("pw5");
+		u5.setUserEmail("email5");
+		u5.setUserName("name5");
+		u5.setUserGender("m");
+		
+		list = new ArrayList<User>();
+		list.add(u1);
+		list.add(u2);
+		list.add(u3);
+		list.add(u4);
+		list.add(u5);
+	}
+	
+	@Test
+	public void joinTest() {
+		for(User u:list) {
+			userDao.join(u);
+		}
+		
+		ArrayList<User> dbList = userDao.searchAll();
+		compareUserList(list, dbList);
+	}
+	
+	@Test
+	public void updateTest() {
+		for(User u:list) {
+			userDao.join(u);
+		}
+		
+		User u = list.get(0);
+		u.setUserName("updatedName");
+		u.setUserEmail("updatedEmail");
+		u.setUserPassword("updatedPassword");
+		
+		userDao.update(u);
+		
+		User updatedUser = userDao.search(u.getUserID());
+		
+		compareUser(u,updatedUser);
+	}
+	
+	@Test
+	public void deleteTest() {
+		for(User u:list) {
+			userDao.join(u);
+		}
+		
+		User u = list.get(0);
+		list.remove(0);
+		userDao.delete(u);
+		
+		ArrayList<User> dbList = userDao.searchAll();
+		
+		compareUserList(list, dbList);
+	}
+	
+	private void compareUserList(ArrayList<User> list1, ArrayList<User> list2) {
+		assertThat(list1.size(),is(list2.size()));
+		
+		for(int i=0;i<list1.size();i++) {
+			compareUser(list1.get(i),list2.get(i));
+		}
+	}
+	
+	private void compareUser(User u1, User u2) {
+		assertThat(u1.getUserID(), is(u2.getUserID()));
+		assertThat(u1.getUserPassword(), is(u2.getUserPassword()));
+		assertThat(u1.getUserName(), is(u2.getUserName()));
+		assertThat(u1.getUserEmail(), is(u2.getUserEmail()));
+		assertThat(u1.getUserGender(), is(u2.getUserGender()));
+	}
+}
